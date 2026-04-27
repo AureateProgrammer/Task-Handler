72a8b114dbe4247faf0dcd993501772a3be7a494
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-27 13:08:12 -0400

    Crating taskFilter Compo

--- task-manager/components/TaskFilter/TaskFilter.tsx
+++ task-manager/components/TaskFilter/TaskFilter.tsx
@@ -1,6 +1,7 @@
 import react from 'react';
 import {taskFilterProps} from '../types';
 
-export const TaskFilter: React.FC<taskFilterProps> = ({filter, setFilter}) => {
-    
-}
+export const TaskFilter: React.FC<taskFilterProps> = ({onFilterChange}) => {
+  const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    onFilterChange({status: e.target.value as any});
+  };}
